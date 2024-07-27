569c0c872170e43d61143923439cbfe8dd82b61f
Ionut Gherman <ghermanionut96@gmail.com>
2024-07-27 12:58:07 +0300

    zuma: enable VoLTE, VoWiFi, Video Calling, RCS & 5G for non-supported countries
    Signed-off-by: Ionut Gherman <ghermanionut96@gmail.com>

--- device-common.mk
+++ device-common.mk
@@ -25,6 +25,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml
 
+TARGET_SYSTEM_PROP := device/google/zuma/system.prop
+
 # Set system properties identifying the chipset
 PRODUCT_VENDOR_PROPERTIES += ro.soc.manufacturer=Google
 TARGET_VENDOR_PROP += device/google/zuma/vendor.prop
