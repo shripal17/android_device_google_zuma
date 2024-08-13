9b7b9b51184d7012e01cfde56f39a5d50bdfe0ac
Ionut Gherman <ghermanionut96@gmail.com>
2024-08-13 11:42:01 +0300

    zuma: enable UDFPS icons and animations
    Signed-off-by: Ionut Gherman <ghermanionut96@gmail.com>

--- device.mk
+++ device.mk
@@ -53,6 +53,9 @@ DISABLE_ARTIFACT_PATH_REQUIREMENTS := true
 # Blur
 TARGET_ENABLE_BLUR := true
 
+# UDFPS
+TARGET_HAS_UDFPS := true
+
 AB_OTA_POSTINSTALL_CONFIG += \
 	RUN_POSTINSTALL_system=true \
 	POSTINSTALL_PATH_system=system/bin/otapreopt_script \
