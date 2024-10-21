972f46eaf3da89aa3d791c896f0e71a2bbd2ba0c
Jis G Jacob <studiokeys@blissroms.org>
2024-10-21 15:52:27 +0530

    zuma: Bringup for BlissROMs
    Signed-off-by: Shripal Jain <shripal17@gmail.com>

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -9,7 +9,7 @@ TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_DEADLINE := true
 TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_TOGGLE := false
 
 # Manifests
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/lineage/config/device_framework_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/bliss/config/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE += \
     device/google/zuma/android.hardware.security.rkp-service.citadel.xml \
     device/google/zuma/manifest_radio_ds.xml
@@ -34,7 +34,7 @@ AB_OTA_PARTITIONS += \
     vbmeta_vendor
 
 # Reserve space for gapps install
--include vendor/lineage/config/BoardConfigReservedSize.mk
+-include vendor/bliss/config/BoardConfigReservedSize.mk
 
 # Verified Boot
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
